d4f8dd97c8e4a4200427c96e2a8caf84dfb5ab0f
OpsDesk CTF <ctf@opsdesk.local>
2026-02-12 14:48:21 +0400

    Initial commit - OpsDesk CTF

--- push-to-github.sh
+++ push-to-github.sh
@@ -11,7 +11,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m'
 
 echo -e "${YELLOW}[*] OpsDesk CTF - GitHub Push Script${NC}"
-echo -e "${YELLOW}[*] Target: github.com/adilaxmdv/vul-web-CPA${NC}"
+echo -e "${YELLOW}[*] Target: github.com/adilaxmdv/vul-web-CPAI${NC}"
 echo ""
 
 # Check directory
